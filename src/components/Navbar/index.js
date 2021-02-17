1f394ba463bf057ebc0b05db8ad9a14066c8a512
Lucas <lmachado72@outlook.com>
2021-02-17 20:04:33 -0300

    Provider

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,35 +1,39 @@
 import React from 'react';
 
 import { Products, Developers, Company } from '../Content';
-import { DropdownOption } from '../DropDown';
+import { DropdownOption, DropdownProvider, DropdownRoot } from '../DropDown';
 import { Container, DropdownStyles } from './styles';
 
 function Navbar() {
   return (
-    <DropdownStyles>
-      <Container>
-        <ul>
-          <li>
-            <DropdownOption 
-              name="Produtos"
-              content={Products}
-            />
-          </li>
-          <li>
-            <DropdownOption 
-              name="Desenvolvedores"
-              content={Developers}
-            />
-          </li>
-          <li>
-            <DropdownOption 
-              name="Empresa"
-              content={Company}
-            />
-          </li>
-        </ul>
-      </Container>
-    </DropdownStyles>
+    <DropdownProvider>
+      <DropdownStyles>
+        <Container>
+          <ul>
+            <li>
+              <DropdownOption 
+                name="Produtos"
+                content={Products}
+              />
+            </li>
+            <li>
+              <DropdownOption 
+                name="Desenvolvedores"
+                content={Developers}
+              />
+            </li>
+            <li>
+              <DropdownOption 
+                name="Empresa"
+                content={Company}
+              />
+            </li>
+          </ul>
+        </Container>
+
+        <DropdownRoot />
+      </DropdownStyles>
+    </DropdownProvider>  
   );
 }
 
